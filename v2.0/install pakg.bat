c032e3d3450b405a1f26852771096938f8f33058
Mzee <798061533@qq.com>
2023-10-12 23:03:45 +0800

    update

--- v2.0/install pakg.bat
+++ v2.0/install pakg.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
-echo 正在安装 Python 包...
+echo 正在安装 Python 依赖包...
 echo 请稍等...
 
 :: 安装 matplotlib
@@ -12,8 +12,26 @@ pip install pandas
 :: 安装 numpy
 pip install numpy
 
+:: 安装 openpyxl
+pip install openpyxl
+
+:: 安装 subprocess
+pip install subprocess
+
+:: 安装 tkinter
+pip install tkinter
+
+:: 安装 ctypes
+pip install ctypes
+
+:: 安装 os
+pip install os
+
+:: 安装 re
+pip install re
+
 echo.
-echo Python 包安装完成。
+echo Python 依赖包安装完成。
 
 :: 暂停以等待用户确认
 pause
